4253713861733967c8c90389dbfe1dd8e49cc4f2
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-09 15:07:54 +0100

    aggiunta testSubtraction() errata

--- src/test/java/org/lessons/java/calculator/CalculatorTest.java
+++ src/test/java/org/lessons/java/calculator/CalculatorTest.java
@@ -19,7 +19,14 @@ class CalculatorTest {
 	@DisplayName("Addition Test")
 	void testAdd() {
 		float sum = c.add(1.5f, 3.75f);
-		assertEquals(5, sum, "La somma deve essere 5");
+		assertEquals(5f, sum, "La somma deve essere 5");
+	}
+
+	@Test
+	@DisplayName("Subtraction Test")
+	void testSubtraction() {
+		float sub = c.subtract(3.5f, 51.5f);
+		assertEquals(2f, sub, "Il risultato dev'essere 2");
 	}
 
 }
