39fb19489c0045a984c03c088948306dc8a52213
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-09 15:12:35 +0100

    Esecuzione di tutti i test correttamente

--- src/test/java/org/lessons/java/calculator/CalculatorTest.java
+++ src/test/java/org/lessons/java/calculator/CalculatorTest.java
@@ -18,28 +18,28 @@ class CalculatorTest {
 	@Test
 	@DisplayName("Addition Test")
 	void testAdd() {
-		float sum = c.add(1.5f, 3.75f);
+		float sum = c.add(1.5f, 3.5f);
 		assertEquals(5f, sum, "La somma deve essere 5");
 	}
 
 	@Test
 	@DisplayName("Subtraction Test")
 	void testSubtract() {
-		float sub = c.subtract(3.5f, 51.5f);
+		float sub = c.subtract(3.5f, 1.5f);
 		assertEquals(2f, sub, "Il risultato dev'essere 2");
 	}
 
 	@Test
 	@DisplayName("Division Test")
 	void testDivide() {
-		float quo = c.divide(6f, .75f);
+		float quo = c.divide(6f, .5f);
 		assertEquals(12f, quo, "Il quoziente dev'essere 12");
 	}
 
 	@Test
 	@DisplayName("Moltiplication Test")
 	void testMultiply() {
-		float pro = c.multiply(6f, .75f);
+		float pro = c.multiply(6f, .5f);
 		assertEquals(3f, pro, "Il prodotto dev'essere 3");
 	}
 
